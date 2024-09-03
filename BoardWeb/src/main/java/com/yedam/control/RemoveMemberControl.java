fa2dbdc02cfabf8ba5712d59a8fde5a0a198fd4e
admin <admin@DESKTOP-5OED7O0>
2024-09-03 16:57:19 +0900

    충돌테스트 전.

---
+++ BoardWeb/src/main/java/com/yedam/control/RemoveMemberControl.java
@@ -0,0 +1,40 @@
+package com.yedam.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.common.Control;
+import com.yedam.service.MemberService;
+import com.yedam.service.MemberServiceImpl;
+import com.yedam.vo.MemberVO;
+
+public class RemoveMemberControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// id 파라미터를 받아서 db 삭제처리. 목록이동.
+		response.setContentType("text/html;charset=utf-8");
+
+		String id = request.getParameter("id"); // 사용자의 요청정보중에서 id값을 읽도록 함.
+		 
+
+		MemberService svc = new MemberServiceImpl();
+		// 회원등록이 정상적일 경우 => 회원목록 페이지 출력.
+		// 회원등록이 비정상적일 경우 => 회원등록 페이지 이동 (msg 전달)
+		// 현재 페이지 : addMember.do -> 페이지 재지정 방식 1)forwarding:파라미터전달. 2)redirect:파라미터 불가.
+		if (svc.removeMember(id)) {
+			// PrintWriter out = response.getWriter(); // 출력스트림을 반환.
+			// out.print("등록됨"); // 웹브라우저에 전달.
+			response.sendRedirect("memberList.do");
+		} else {
+			request.setAttribute("message", "등록중에 오류가 있습니다.");
+			request.getRequestDispatcher("WEB-INF/html/memberInfo.jsp")//
+					.forward(request, response);
+		}
+	}
+
+}
--- BoardWeb/src/main/webapp/WEB-INF/html/memberInfo.jsp
+++ BoardWeb/src/main/webapp/WEB-INF/html/memberInfo.jsp
@@ -23,8 +23,9 @@
         	<th>가입일자</th><td><%=member.getCreationDate() %></td>
         </tr>
         <tr>
-        	<td colspan="2" align="center"><button class="btn btn-info" onclick="location.href='modifyForm.do?id=<%=member.getMemberId() %>'">수정</button>
-        	    <button class="btn btn-danger">삭제</button>
+        	<td colspan="2" align="center">
+        	    <button class="btn btn-info" onclick="location.href='modifyForm.do?id=<%=member.getMemberId() %>'">수정</button>
+        	    <button class="btn btn-danger" onclick="location.href='removeMember.do?id=<%=member.getMemberId() %>'">삭제</button>
         	</td>
         </tr>
     </table>
