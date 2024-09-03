be17e9579271606d9c994fc7c6a4ae49f1f76c57
changholee2010 <148027761+changholee2010@users.noreply.github.com>
2024-09-03 16:59:16 +0900

    Update FrontController.java

--- BoardWeb/src/main/java/com/yedam/web/FrontController.java
+++ BoardWeb/src/main/java/com/yedam/web/FrontController.java
@@ -48,6 +48,8 @@ public class FrontController extends HttpServlet {
 		map.put("/modifyMember.do", new ModifyMemberControl()); // 수정처리.
 		map.put("/removeMember.do", new RemoveMemberControl()); // 삭제처리.
 
+		// 나도 수정했어...
+
 	}
 
 	// HttpServletRequest
